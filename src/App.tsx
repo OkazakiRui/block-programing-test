596aaf398009a4eb84128a1407d32ff130779a19
RuiOkazaki <okazaki200226@gmail.com>
2022-04-04 00:00:53 +0900

    add: field

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
-const App = () => {
-  return <div className="App">hoge</div>;
+import { useEffect, VFC } from 'react';
+import Blockly from 'blockly';
+
+const App: VFC = () => {
+  const xml = `
+      <xml id="toolbox">
+          <category name="Loops" colour="%{BKY_LOOPS_HUE}">
+              <block type="controls_repeat_ext">
+                  <value name="TIMES">
+                      <block type="math_number">
+                          <field name="NUM">10</field>
+                      </block>
+                  </value>
+              </block>
+              <block type="controls_whileUntil"></block>
+          </category>
+          <category name="Math" colour="%{BKY_MATH_HUE}">
+              <block type="math_number">
+                  <field name="NUM">123</field>
+              </block>
+          </category>
+          <category name="Text" colour="%{BKY_TEXTS_HUE}">
+              <block type="text"></block>
+              <block type="text_print"></block>
+          </category>
+      </xml>
+  `;
+  const xmlParser = new DOMParser();
+  const xmlDom = xmlParser.parseFromString(xml, 'text/xml');
+
+  useEffect(() => {
+    Blockly.inject('blocklyDiv', {
+      toolbox: xmlDom.getElementById('toolbox'),
+    });
+  });
+
+  return (
+    <div>
+      <div id="blocklyDiv" style={{ width: '1024px', height: '600px' }}></div>
+    </div>
+  );
 };
 
 export default App;
